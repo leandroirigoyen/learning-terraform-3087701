0fb1512f986b432d6d2a0ed5b67ed145698bf94f
Leandro Irigoyen <98347893+leandroirigoyen@users.noreply.github.com>
2025-05-10 00:48:46 -0300

    load balancer correction

--- main.tf
+++ main.tf
@@ -48,42 +48,43 @@ resource "aws_instance" "blog" {
 }
 
 module "alb" {
-  source = "terraform-aws-modules/alb/aws"
+  source  = "terraform-aws-modules/alb/aws"
+  version = "9.16.0"
 
-  name    = "blog-alb"
-
+  name               = "blog-alb"
   load_balancer_type = "application"
 
-  vpc_id             = module.blog_vpc.vpc_id
-  subnets            = module.blog_vpc.public_subnets
-  security_groups    = module.blog_sg.security_group_id
+  vpc_id          = module.blog_vpc.vpc_id
+  subnets         = module.blog_vpc.public_subnets
+  security_groups = module.blog_sg.security_group_id
 
-  target_groups = {
-      name_prefix      = "blog"
-      protocol         = "HTTP"
-      port             = 80
+  target_groups = [
+    {
+      name_prefix = "blog"
+      backend_protocol = "HTTP"
+      backend_port     = 80
       target_type      = "instance"
-      targets = {
-        my_target = {
-          target_id        = aws_instance.blog.id
-          port = 80
+      targets = [
+        {
+          target_id = aws_instance.blog.id
+          port      = 80
         }
-      }
+      ]
     }
-  }
+  ]
 
-  http_tcp_listeners = [
+  listeners = [
     {
       port               = 80
       protocol           = "HTTP"
       target_group_index = 0
-
     }
   ]
 
-  tags = {
+  common_tags = {
     Environment = "dev"
   }
+}
 
 
 module "blog_sg" {
