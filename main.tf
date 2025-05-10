bba31360aed7a8006054e869233de7ec1ebb3c8f
Leandro Irigoyen <98347893+leandroirigoyen@users.noreply.github.com>
2025-05-10 00:55:19 -0300

    load ablaancer

--- main.tf
+++ main.tf
@@ -47,33 +47,28 @@ resource "aws_instance" "blog" {
   }
 }
 
-module "alb" {
+module "blog_alb" {
   source  = "terraform-aws-modules/alb/aws"
-  version = "9.16.0"
+  version = "~> 6.0"
 
-  name               = "blog-alb"
+  name = "blog-alb"
+
   load_balancer_type = "application"
 
-  vpc_id          = module.blog_vpc.vpc_id
-  subnets         = module.blog_vpc.public_subnets
-  security_groups = module.blog_sg.security_group_id
+  vpc_id             = module.blog_vpc.vpc_id
+  subnets            = module.blog_vpc.public_subnets
+  security_groups    = [module.blog_sg.security_group_id]
 
   target_groups = [
     {
-      name_prefix = "blog"
+      name_prefix      = "blog-"
       backend_protocol = "HTTP"
       backend_port     = 80
       target_type      = "instance"
-      targets = [
-        {
-          target_id = aws_instance.blog.id
-          port      = 80
-        }
-      ]
     }
   ]
 
-  listeners = [
+  http_tcp_listeners = [
     {
       port               = 80
       protocol           = "HTTP"
@@ -85,6 +80,7 @@ module "alb" {
     Environment = "dev"
   }
 }
+
 
 
 module "blog_sg" {
